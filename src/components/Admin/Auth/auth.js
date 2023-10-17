d6291664280c2541a97359dd2789596603612262
Ahtesanul Hoque <134125661+kaiser-hoque2@users.noreply.github.com>
2023-10-17 12:53:36 +0600

    Login And Register done

--- src/components/Admin/Auth/auth.js
+++ src/components/Admin/Auth/auth.js
@@ -1,33 +1,33 @@
 import axios from 'axios';
-const SERVER_URL = "http://localhost/restApis";
+// const SERVER_URL = "http://localhost/restApis";
 
 const login = async (data) => {
-    const LOGIN_ENDPOINT = `${SERVER_URL}/login.php`;
+    const LOGIN_ENDPOINT = `${global.config.apiUrl}auth/login`;
     try {
         let response = await axios.post(LOGIN_ENDPOINT, data);
-        if(response.data.jwt){
-            localStorage.setItem("access_token", response.data.jwt);
-            localStorage.setItem("userdata", response.data.datas);
+        if (response.data.data.jwt) {
+            localStorage.setItem("access_token", response.data.data.jwt);
+            localStorage.setItem("userdata", response.data.data.datas);
             return true;
-        }else{
+        } else {
             return false;
         }
-    } catch(e){
+    } catch (e) {
         return false;
-        // console.log(e);
+        console.log(e);
     }
 }
 
 const register = async (data) => {
-    const SIGNUP_ENDPOINT = `${SERVER_URL}/register.php`;
+    const SIGNUP_ENDPOINT = `${global.config.apiUrl}auth/register`;
     try {
         let response = await axios({
             method: 'post',
             responseType: 'json',
             url: SIGNUP_ENDPOINT,
             data: data
-          });
-    } catch(e){
+        });
+    } catch (e) {
         console.log(e);
     }
 }
