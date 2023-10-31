d328518867a6280e112eed1b0ee14e6bc2c817f3
Ahtesanul Hoque <134125661+kaiser-hoque2@users.noreply.github.com>
2023-10-31 10:08:16 +0600

    Last Commit

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -4,7 +4,7 @@ import Header from '../Header/header';
 import Footer from '../Footer/footer';
 import CounterUp from '../CounterUp/counterUp';
 
-const About = ({ showHeader = true, showFooter = true, showTeam = true, showHero = true}) => {
+const About = ({ showHeader = true, showFooter = true, showTeam = true, showHero = true }) => {
     const [teamData, setTeamData] = useState([]);
 
     useEffect(() => {
@@ -29,22 +29,22 @@ const About = ({ showHeader = true, showFooter = true, showTeam = true, showHero
                     {showHeader && (
                         <Header />
                     )}
-                     {showHero && (
-                    <div className="container-xxl py-5 bg-dark hero-header mb-5">
-                        <div className="container text-center my-5 pt-5 pb-4">
-                            <h1 className="display-3 text-white mb-3 animated slideInDown">About Us</h1>
-                            <nav aria-label="breadcrumb">
-                                <ol className="breadcrumb justify-content-center text-uppercase">
-                                    <li className="breadcrumb-item"><a href="./">Home</a></li>
-                                    <li className="breadcrumb-item"><a href="./">Pages</a></li>
-                                    <li className="breadcrumb-item text-white active" aria-current="page">About</li>
-                                </ol>
-                            </nav>
+                    {showHero && (
+                        <div className="container-xxl py-5 bg-dark hero-header mb-5">
+                            <div className="container text-center my-5 pt-5 pb-4">
+                                <h1 className="display-3 text-white mb-3 animated slideInDown">About Us</h1>
+                                <nav aria-label="breadcrumb">
+                                    <ol className="breadcrumb justify-content-center text-uppercase">
+                                        <li className="breadcrumb-item"><a href="./">Home</a></li>
+                                        <li className="breadcrumb-item"><a href="./">Pages</a></li>
+                                        <li className="breadcrumb-item text-white active" aria-current="page">About</li>
+                                    </ol>
+                                </nav>
+                            </div>
                         </div>
-                    </div>
                     )}
                 </div>
-                
+
                 {/* <!-- Navbar & Hero End --> */}
 
 
@@ -93,35 +93,35 @@ const About = ({ showHeader = true, showFooter = true, showTeam = true, showHero
                 {/* <!-- About End --> */}
 
                 {showTeam && (
-                <div className="container-xxl pt-5 pb-3">
-                    <div className="container">
-                        <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
-                            <h5 className="section-title ff-secondary text-center text-primary fw-normal">Team Members</h5>
-                            <h1 className="mb-5">Our Master Chefs</h1>
-                        </div>
-                        <div className="row g-4">
-                            {teamData.map((teamMember) => (
-                                <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                                    <div className="team-item text-center rounded overflow-hidden">
-                                        <div className="rounded-circle overflow-hidden m-4">
-                                            <img className="img-fluid" src={teamMember.imageSrc} alt="" />
-                                        </div>
-                                        <h5 className="mb-0">{teamMember.name}</h5>
-                                        <small>{teamMember.designation}</small>
-                                        <div className="d-flex justify-content-center mt-3">
-                                            <a className="btn btn-square btn-primary mx-1" href="./"><i className="fab fa-facebook-f"></i></a>
-                                            <a className="btn btn-square btn-primary mx-1" href="./"><i className="fab fa-twitter"></i></a>
-                                            <a className="btn btn-square btn-primary mx-1" href="./"><i className="fab fa-instagram"></i></a>
+                    <div className="container-xxl pt-5 pb-3">
+                        <div className="container">
+                            <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
+                                <h5 className="section-title ff-secondary text-center text-primary fw-normal">Team Members</h5>
+                                <h1 className="mb-5">Our Master Chefs</h1>
+                            </div>
+                            <div className="row g-4">
+                                {teamData.map((teamMember) => (
+                                    <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                                        <div className="team-item text-center rounded overflow-hidden">
+                                            <div className="rounded-circle overflow-hidden m-4">
+                                                <img className="img-fluid" src={teamMember.imageSrc} alt="" />
+                                            </div>
+                                            <h5 className="mb-0">{teamMember.name}</h5>
+                                            <small>{teamMember.designation}</small>
+                                            <div className="d-flex justify-content-center mt-3">
+                                                <a className="btn btn-square btn-primary mx-1" href="./"><i className="fab fa-facebook-f"></i></a>
+                                                <a className="btn btn-square btn-primary mx-1" href="./"><i className="fab fa-twitter"></i></a>
+                                                <a className="btn btn-square btn-primary mx-1" href="./"><i className="fab fa-instagram"></i></a>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            ))}
+                                ))}
+                            </div>
                         </div>
                     </div>
-                </div>
                 )}
                 {/* <!-- Team End --> */}
-                
+
                 {showFooter && (
                     <Footer />
                 )}
